65de3903704b3aa88b6882c01d25fb697ebb4e65
itachi1706 <kennethsohyq@hotmail.com>
2015-06-01 16:37:02 +0800

    Updated Manifest for Lollipop - Added test button to test the stuff

--- app/src/main/java/com/itachi1706/droideggs/MainScreen.java
+++ app/src/main/java/com/itachi1706/droideggs/MainScreen.java
@@ -1,16 +1,32 @@
 package com.itachi1706.droideggs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.itachi1706.droideggs.LollipopEgg.PlatLogoActivityLOLLIPOP;
 
 public class MainScreen extends AppCompatActivity {
+
+    Button test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        test = (Button) findViewById(R.id.btnTest);
+
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainScreen.this, PlatLogoActivityLOLLIPOP.class));
+            }
+        });
     }
 
     @Override

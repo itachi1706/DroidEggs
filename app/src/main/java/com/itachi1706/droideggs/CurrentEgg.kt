b35d2b0aae5acc89847f383fed9636bcbf85e96c
itachi1706 <kennethsohyq@gmail.com>
2019-12-14 22:32:04 +0800

    DEGGAND-45 Refactored the Current Egg code so that its easier to manage and execute the code
    This also reduces code complexity hopefully

--- app/src/main/java/com/itachi1706/droideggs/CurrentEgg.kt
+++ app/src/main/java/com/itachi1706/droideggs/CurrentEgg.kt
@@ -17,10 +17,13 @@ package com.itachi1706.droideggs
 import android.app.Activity
 import android.content.Intent
 import android.os.Build
+import android.os.Build.VERSION_CODES.*
 import android.os.Bundle
-import android.preference.PreferenceManager
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.itachi1706.droideggs.GingerbreadEgg.PlatLogoActivityGINGERBREAD
+import com.itachi1706.droideggs.HoneycombEgg.PlatLogoActivityHONEYCOMB
+import com.itachi1706.droideggs.IceCreamSandwichEgg.PlatLogoActivityICS
 import com.itachi1706.droideggs.JellyBeanEgg.PlatLogoActivityJELLYBEAN
 import com.itachi1706.droideggs.KitKatEgg.PlatLogoActivityKITKAT
 import com.itachi1706.droideggs.LollipopEgg.PlatLogoActivityLOLLIPOP
@@ -40,28 +43,26 @@ class CurrentEgg : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val errorIntent = Intent()
-        when {
-            Build.VERSION.SDK_INT >= 99999 -> errorIntent.putExtra("class", "comingsoon") // ??? (Future P)
-            //Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q -> startActivity(Intent(this, PlatLogoActivityQ::class.java)) // Q TODO: Use this when targeting Q
-            Build.VERSION.SDK_INT >= 29 -> startActivity(Intent(this, PlatLogoActivityQ::class.java)) // Q TODO: Remove when targeting Q
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.P -> startActivity(Intent(this, PlatLogoActivityPie::class.java)) // Pie
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1 -> startActivity(Intent(this, PlatLogoActivityOreoMR1::class.java)) // Nougat
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.O -> startActivity(Intent(this, PlatLogoActivityOreo::class.java)) // Nougat
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.N -> { // Nougat
-                val i = Intent(this, PlatLogoActivityNougat::class.java)
-                i.putExtra("setting", PreferenceManager.getDefaultSharedPreferences(this).getBoolean("actual_neko_egg", false))
-                startActivity(i)
-            }
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.M -> startActivity(Intent(this, PlatLogoActivityMARSHMALLOW::class.java)) //Marshmallow
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP -> startActivity(Intent(this, PlatLogoActivityLOLLIPOP::class.java)) //Lollipop (21-22)
-            Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT -> startActivity(Intent(this, PlatLogoActivityKITKAT::class.java)) //KitKat
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN -> startActivity(Intent(this, PlatLogoActivityJELLYBEAN::class.java)) //Jelly Bean
-            else -> errorIntent.putExtra("class", "noegg") // No Egg
+        val errorIntent = Intent().apply { putExtra("class", "comingsoon") }
+        val executeClass = when (Build.VERSION.SDK_INT) {
+            GINGERBREAD, GINGERBREAD_MR1 -> PlatLogoActivityGINGERBREAD::class.java // Gingerbread
+            HONEYCOMB, HONEYCOMB_MR1, HONEYCOMB_MR2 -> PlatLogoActivityHONEYCOMB::class.java // Honeycomb
+            ICE_CREAM_SANDWICH, ICE_CREAM_SANDWICH_MR1 -> PlatLogoActivityICS::class.java // Ice Cream Sandwich
+            JELLY_BEAN, JELLY_BEAN_MR1, JELLY_BEAN_MR2 -> PlatLogoActivityJELLYBEAN::class.java // Jelly Bean
+            KITKAT, KITKAT_WATCH -> PlatLogoActivityKITKAT::class.java // Kit Kat
+            LOLLIPOP, LOLLIPOP_MR1 -> PlatLogoActivityLOLLIPOP::class.java // Lollipop
+            M -> PlatLogoActivityMARSHMALLOW::class.java // Marshmallow
+            N, N_MR1 -> PlatLogoActivityNougat::class.java // Nougat
+            O -> PlatLogoActivityOreo::class.java // Oreo
+            O_MR1 -> PlatLogoActivityOreoMR1::class.java // Oreo
+            P -> PlatLogoActivityPie::class.java // Pie
+            29 -> PlatLogoActivityQ::class.java // Android 10 (Q) // TODO: Replace 29 with Q when targeting Q
+            else -> null // Future Android Versions (As minSDK is way above GB, it does not matter to do a check before)
         }
 
-        if (errorIntent.hasExtra("class")) setResult(Activity.RESULT_CANCELED, errorIntent)
+        if (executeClass == null) setResult(Activity.RESULT_CANCELED, errorIntent)
         else {
+            startActivity(Intent(this, executeClass))
             setResult(Activity.RESULT_OK)
             FirebaseLogger.logFirebase(this, "SDK: ${Build.VERSION.SDK_INT}", "current_egg_selected") // Firebase Analytics Event Logging
             Log.i("Firebase", "Logged Current Egg Selected for SDK " + Build.VERSION.SDK_INT)

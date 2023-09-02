07091cee2e148baa9e1839db4d5df1aa01d0e651
Kenneth Soh <kenneth@itachi1706.com>
2023-09-02 22:02:11 +0800

    refactor: Removed invalid SonarQube warnings
    Took 13 minutes

--- app/src/main/java/com/itachi1706/droideggs/MarshmallowEgg/MLand.java
+++ app/src/main/java/com/itachi1706/droideggs/MarshmallowEgg/MLand.java
@@ -353,19 +353,19 @@ public class MLand extends FrameLayout {
         mHeight = getHeight();
         boolean showingSun = (mTimeOfDay == DAY || mTimeOfDay == SUNSET) && frand() > 0.25;
         if (showingSun) {
-            final Star sun = new Star(getContext());
-            sun.setBackgroundResource(R.drawable.marshmallow_sun);
+            final Star bgSun = new Star(getContext());
+            bgSun.setBackgroundResource(R.drawable.marshmallow_sun);
             final int w = getResources().getDimensionPixelSize(R.dimen.sun_size);
-            sun.setTranslationX(frand(w, mWidth-w));
+            bgSun.setTranslationX(frand(w, mWidth-w));
             if (mTimeOfDay == DAY) {
-                sun.setTranslationY(frand(w, (mHeight * 0.66f)));
-                sun.getBackground().setTint(0);
+                bgSun.setTranslationY(frand(w, (mHeight * 0.66f)));
+                bgSun.getBackground().setTint(0);
             } else {
-                sun.setTranslationY(frand(mHeight * 0.66f, mHeight - w));
-                sun.getBackground().setTintMode(PorterDuff.Mode.SRC_ATOP);
-                sun.getBackground().setTint(0xC0FF8000);
+                bgSun.setTranslationY(frand(mHeight * 0.66f, mHeight - w));
+                bgSun.getBackground().setTintMode(PorterDuff.Mode.SRC_ATOP);
+                bgSun.getBackground().setTint(0xC0FF8000);
             }
-            addView(sun, new LayoutParams(w, w));
+            addView(bgSun, new LayoutParams(w, w));
         }
         if (!showingSun) {
             final boolean dark = mTimeOfDay == NIGHT || mTimeOfDay == TWILIGHT;

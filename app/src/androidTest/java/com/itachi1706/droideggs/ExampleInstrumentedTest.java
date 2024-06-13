3047c986023053ca190f6e662800baa7da28626b
Kenneth Soh <kenneth@itachi1706.com>
2024-06-13 20:06:58 +0800

    refactor: Updated dependencies for testing

--- app/src/androidTest/java/com/itachi1706/droideggs/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/itachi1706/droideggs/ExampleInstrumentedTest.java
@@ -1,13 +1,14 @@
 package com.itachi1706.droideggs;
 
+import static org.junit.Assert.assertEquals;
+
 import android.content.Context;
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
 
-import org.junit.Test;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -15,11 +16,11 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() throws Exception {
+    void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.itachi1706.droideggs", appContext.getPackageName());
     }
